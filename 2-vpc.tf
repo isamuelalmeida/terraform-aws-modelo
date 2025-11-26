796b18084930ceec1cdc56c42204605da79b3b39
Samuel Mota <samuel.mota@cogna.com.br>
2025-11-26 02:35:39 -0300

    nova vpc module

--- 2-vpc.tf
+++ 2-vpc.tf
@@ -1,10 +1,33 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.100.0.0/16"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.5.1"
 
-  enable_dns_support = true
+  name = "${local.env}-vpc"
+  cidr = "10.200.0.0/16"
+
+  azs             = [local.zone1, local.zone2]
+  private_subnets = ["10.200.0.0/19", "10.200.32.0/19"]
+  public_subnets  = ["10.200.64.0/19", "10.200.96.0/19"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  map_public_ip_on_launch = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                                = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"                       = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
 
   tags = {
-    Name = "${local.env}-vpc"
+    Environment = local.env
+    Terraform   = "true"
   }
 }
